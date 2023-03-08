c5fa1ebce441a1934e6e6e32f0807d53203e4704
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2023-03-08 22:21:22 +0200

    first commit

---
+++ lib/calculator.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:calculator/element_style.dart';
+
+class Calculator extends StatelessWidget {
+  Calculator({super.key});
+  final List buttonsList = [
+    'C',
+    /* const Icon(
+      Icons.backspace_rounded,
+      size: 2,
+    ) */
+    'C',
+    '⌫',
+    '%',
+    '÷',
+    '7',
+    '8',
+    '9',
+    '×',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '|',
+    '0',
+    '.',
+    '=',
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * (2 / 5) - 5,
+            child: Expanded(
+              child: Card(
+                color: Theme.of(context).primaryColor,
+                child: const Expanded(
+                  child: Text("data"),
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            height: 5,
+            thickness: 1,
+            indent: 20,
+            endIndent: 20,
+            color: Color.fromARGB(255, 224, 224, 224),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    ElementBox(element: buttonsList[0]),
+                    ElementBox(element: buttonsList[1]),
+                    ElementBox(element: buttonsList[2]),
+                    ElementBox(element: buttonsList[3]),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ElementBox(element: buttonsList[4]),
+                    ElementBox(element: buttonsList[5]),
+                    ElementBox(element: buttonsList[6]),
+                    ElementBox(element: buttonsList[7]),
+                  ],
+                ),
+                Row(
+                  children: const [
+                    ElementBox(element: " "),
+                    ElementBox(element: " "),
+                    ElementBox(element: " "),
+                    ElementBox(element: " "),
+                  ],
+                ),
+                Row(
+                  children: const [
+                    ElementBox(element: " "),
+                    ElementBox(element: " "),
+                    ElementBox(element: " "),
+                    ElementBox(element: " "),
+                  ],
+                ),
+                Row(
+                  children: const [
+                    ElementBox(element: " "),
+                    ElementBox(element: " "),
+                    ElementBox(element: " "),
+                    ElementBox(element: " "),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/element_style.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ElementBox extends StatelessWidget {
+  const ElementBox({super.key, required this.element});
+  final element;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * (3 / 5) / 5,
+      width: MediaQuery.of(context).size.width / 4,
+      child: Container(
+        alignment: Alignment.center,
+        color: Theme.of(context).primaryColor,
+        child: Expanded(
+          child: Text(
+            element,
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+      ),
+    );
+  }
+}
