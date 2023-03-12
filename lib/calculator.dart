536a7039bb81be74f966f2d57a1ceec8559232b3
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2023-03-12 20:26:19 +0200

    calculator class using gridview.build

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -31,12 +31,23 @@ class Calculator extends StatelessWidget {
       body: Column(
         children: [
           SizedBox(
-            height: MediaQuery.of(context).size.height * (2 / 5) - 5,
+            height: MediaQuery.of(context).size.height * (1 / 5) - 5,
             child: Expanded(
               child: Card(
                 color: Theme.of(context).primaryColor,
                 child: const Expanded(
                   child: Text("data"),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * (1 / 5),
+            child: Expanded(
+              child: Card(
+                color: Theme.of(context).primaryColor,
+                child: const Expanded(
+                  child: Text("result"),
                 ),
               ),
             ),
@@ -48,52 +59,21 @@ class Calculator extends StatelessWidget {
             endIndent: 20,
             color: Color.fromARGB(255, 224, 224, 224),
           ),
-          Expanded(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    ElementBox(element: buttonsList[0]),
-                    ElementBox(element: buttonsList[1]),
-                    ElementBox(element: buttonsList[2]),
-                    ElementBox(element: buttonsList[3]),
-                  ],
-                ),
-                Row(
-                  children: [
-                    ElementBox(element: buttonsList[4]),
-                    ElementBox(element: buttonsList[5]),
-                    ElementBox(element: buttonsList[6]),
-                    ElementBox(element: buttonsList[7]),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    ElementBox(element: " "),
-                    ElementBox(element: " "),
-                    ElementBox(element: " "),
-                    ElementBox(element: " "),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    ElementBox(element: " "),
-                    ElementBox(element: " "),
-                    ElementBox(element: " "),
-                    ElementBox(element: " "),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    ElementBox(element: " "),
-                    ElementBox(element: " "),
-                    ElementBox(element: " "),
-                    ElementBox(element: " "),
-                  ],
-                ),
-              ],
+          SizedBox(
+            height: MediaQuery.of(context).size.height * (3 / 5),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+              ),
+              shrinkWrap: true,
+              itemCount: buttonsList.length,
+              itemBuilder: (context, index) {
+                return ElementBox(
+                  element: buttonsList[index],
+                );
+              },
             ),
-          ),
+          )
         ],
       ),
     );
