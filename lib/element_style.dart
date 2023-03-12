7f3cbc786810e94aa97a25de427c209e4ca43dc7
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2023-03-12 21:41:51 +0200

    overall style changed and equalSign class added

--- lib/element_style.dart
+++ lib/element_style.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ElementBox extends StatelessWidget {
-  const ElementBox({super.key, required this.element});
-  final element;
+  const ElementBox({super.key, required this.element, required this.textColor});
+  final String element;
+  final Color textColor;
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +12,35 @@ class ElementBox extends StatelessWidget {
       width: MediaQuery.of(context).size.width / 4,
       child: Container(
         alignment: Alignment.center,
-        color: Theme.of(context).primaryColor,
-        child: Expanded(
-          child: Text(
-            element,
-            style: const TextStyle(fontSize: 22),
+        child: Text(
+          element,
+          style: TextStyle(
+            fontSize: 24,
+            color: textColor,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class EqualSign extends StatelessWidget {
+  const EqualSign({super.key, required this.element});
+  final String element;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * (3 / 5) / 5 - 20,
+      width: MediaQuery.of(context).size.width / 4,
+      alignment: Alignment.center,
+      color: Theme.of(context).primaryColor,
+      margin: const EdgeInsets.all(10),
+      child: Text(
+        element,
+        style: const TextStyle(
+          fontSize: 34,
+          color: Colors.white,
         ),
       ),
     );
